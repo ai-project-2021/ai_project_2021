0d01fefc9d16e597b99d3b8cf994da18c9dca7d5
Donghyun Kim <eugene6124@gmail.com>
2021-05-25 20:19:51 +0900

    feat: add clustering evaluate method

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
-from .loader import get_fraud, get_product_recommend, get_rfm_data
+from .loader import get_fraud, get_product_recommend, get_rfm_data, get_rfm_score
+from .metrics import inertia
 
-__all__ = ["get_fraud", "get_product_recommend", "get_rfm_data"]
+__all__ = ["get_fraud", "get_product_recommend", "get_rfm_data", "inertia"]
