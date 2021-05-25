25bb1b82dcad842aa2323f8d00bb35e5d0a7ac2c
Donghyun Kim <eugene6124@gmail.com>
2021-05-26 02:00:26 +0900

    feat: add visualization for fuzzy

--- models/fuzzy.py
+++ models/fuzzy.py
@@ -1,7 +1,14 @@
 import numpy as np
 from sklearn.utils import check_random_state
 from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics import silhouette_samples, silhouette_score
+import argparse
+from tqdm import trange
+import time
+
 from utils import get_product_recommend, get_rfm_data
+from utils.metrics import inertia
+from matplotlib import pyplot as plt
 
 
 def d_tool(X, y):
@@ -50,13 +57,36 @@ class FuzzyKMeans:
             if np.sum((centers_old - self.cluster_centers_) ** 2) < self.threshold * vdata:
                 break
 
+        self.centroid = self.cluster_centers_
+        self.labels_ = self.fuzzy_labels_.argmax(axis=1)
+        self.inertia_ = inertia(X, self.labels_, self.cluster_centers_)
+
         return self
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="K-Centriod Arguments")
+    parser.add_argument("--models", "-m", type=str, action="append", help="K-Centriod Method Type")
+    parser.add_argument("--n_clusters", "-k", type=int, action="append")
+    args = parser.parse_args()
+
     dataset = get_rfm_data(get_product_recommend())[["R_Value", "F_Value", "M_Value"]]
     model = FuzzyKMeans(k=3, m=2, max_iter=100, random_state=0, threshold=1e-6)
 
-    model.fit(dataset.values)
-    labels = model.labels_
-    print(labels)
+    s = time.time()
+    elbow = []
+    n_cols = len(args.n_clusters)
+    fig, axs = plt.subplots(figsize=(4 * n_cols, 4), nrows=1, ncols=n_cols)
+
+    for i, k in enumerate(args.n_clusters):
+        e = time.time()
+        model.k = k
+        elbow.append(model.fit(dataset.values).inertia_)
+        silhouette_avg = silhouette_score(dataset.values, model.labels_)
+        print(
+            "K : {}, Inertia : {}, Avg. Silhouette_Score : {}. RMSE : {}".format(k, elbow[-1], silhouette_avg, np.sqrt(((model.labels_, model.))))
+        )
+
+    plt.figure()
+    plt.plot(args.n_clusters, elbow)
+    plt.savefig(f"./fuzzy.png")
